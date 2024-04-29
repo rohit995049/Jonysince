af7f9c9080b50ef62989feb99caf83721eb2b146
rohit99504 <163815811+rohit99504@users.noreply.github.com>
2024-04-29 13:20:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 from os import getenv
 
 
-API_ID = int(getenv("API_ID", "18618422"))
-API_HASH = getenv("API_HASH", "f165b1caec3cfa4df943fe1cbe82d22a")
-BOT_TOKEN = getenv("BOT_TOKEN", "6331627404:AAFmXuPUywE0aXxBPrL6rEToysrIGZwrQmU")
-OWNER_ID = int(getenv("OWNER_ID", "6050277919"))
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6050277919 2112898623").split()))
+API_ID = int(getenv("API_ID", "25891183"))
+API_HASH = getenv("API_HASH", "36709c81d7609a81f86de931cbc87f3a")
+BOT_TOKEN = getenv("BOT_TOKEN", "6996876631:AAGASPPtUFhFaG2lQbzytdjozKmn0LJWn8k")
+OWNER_ID = int(getenv("OWNER_ID", "2113261793"))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1001992164182").split()))
 MONGO_DB = getenv("MONGO_DB", "mongodb+srv://mohitag24082006:ge3z5sQOsr6NiTTt@cluster0.xxrppe5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002099606627"))
